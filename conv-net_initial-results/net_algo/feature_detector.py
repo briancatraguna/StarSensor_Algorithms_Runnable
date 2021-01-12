aaa7501857a023ae5e61bdad29628104037ec3d2
briancatraguna <briancatraguna@rocketmail.com>
2021-01-12 11:49:55 +0700

    Uploading files

---
+++ conv-net_initial-results/net_algo/feature_detector.py
@@ -0,0 +1,181 @@
+#Importing the necessary libraries
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from math import sqrt
+from operator import itemgetter
+
+def displayImg(img,cmap='gray'):
+    fig = plt.figure(figsize=(12,10))
+    ax = fig.add_subplot(111)
+    ax.imshow(img,cmap)
+    plt.show()
+
+
+#Net Algorithm
+def net_feature(path,n):
+    img = cv2.imread(path)   
+
+    #Find the center of the image
+    height,width,col = img.shape
+    coordinate = [height/2,width/2]
+    y = int(coordinate[0])
+    x = int(coordinate[1])
+    print(x,y)
+
+    #Set up the detector
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByInertia = False
+    params.filterByConvexity = False
+    params.minThreshold = 50
+    params.maxThreshold = 255
+    params.filterByColor = True
+    params.blobColor = 255
+    params.filterByArea = False
+    params.minArea = 1
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    #Detect stars
+    keypoints = detector.detect(img)
+    coord = []
+    for index,keypoint in enumerate(keypoints):
+        x_centralstar = int(round(keypoints[index].pt[0]))
+        y_centralstar = int(round(keypoints[index].pt[1]))
+        distance_to_center = sqrt(((x_centralstar-x)**2)+((y_centralstar-y)**2))
+        coord.append([x_centralstar,y_centralstar,distance_to_center])
+
+    coord = sorted(coord,key=itemgetter(2))
+    coord = coord[:n]
+    for item in coord:
+        cv2.circle(img,center=(item[0],item[1]),radius=2,color=(255,0,0),thickness=2)
+
+    pivot_star_coord = tuple(coord[0][0:2])
+    del coord[0]
+
+    #Draw lines from pivot point to other stars
+    for coordinate in coord:
+        coordinate = coordinate[0:2]
+        cv2.line(img,pivot_star_coord,tuple(coordinate),(255,0,0),2)
+
+    return img
+
+
+#Multitriangles Algorithm
+def multitriangles_detector(path,n):
+    img = cv2.imread(path)  
+
+    #Find the center of the image
+    height,width,col = img.shape
+    coordinate = [height/2,width/2]
+    y = int(coordinate[0])
+    x = int(coordinate[1])
+
+    #Set up the detector
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByInertia = False
+    params.filterByConvexity = False
+    params.minThreshold = 50
+    params.maxThreshold = 255
+    params.filterByColor = True
+    params.blobColor = 255
+    params.filterByArea = False
+    params.minArea = 1
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    #Find the pivot point (Star closest to the center)
+    keypoints = detector.detect(img)
+    coord = []
+    for index,keypoint in enumerate(keypoints):
+        x_centralstar = int(round(keypoints[index].pt[0]))
+        y_centralstar = int(round(keypoints[index].pt[1]))
+        distance_to_center = sqrt(((x_centralstar-x)**2)+((y_centralstar-y)**2))
+        coord.append([x_centralstar,y_centralstar,distance_to_center])
+
+    coord = sorted(coord,key=itemgetter(2))
+    stars_coordinate = coord[:n]
+    for item in stars_coordinate:
+        cv2.circle(img,center=(item[0],item[1]),radius=2,color=(255,0,0),thickness=2)
+
+    for coord in stars_coordinate:
+        coord = tuple(coord[0:2])
+        for other_coord in stars_coordinate:
+            other_coord = tuple(other_coord[0:2])
+            if other_coord == coord:
+                continue
+            else:
+                other_coord = tuple(other_coord)
+                cv2.line(img,coord,other_coord,(255,0,0),2)
+
+    return img
+
+#Circle
+def centroiding(path):
+    img = cv2.imread(path)   
+    print("ORIGINAL SHAPE: ",img.shape)
+
+    #Find the center of the image
+    height,width,col = img.shape
+    coordinate = [height/2,width/2]
+    y = int(coordinate[0])
+    x = int(coordinate[1])
+
+    #Set up the detector
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByInertia = False
+    params.filterByConvexity = False
+    params.minThreshold = 50
+    params.maxThreshold = 255
+    params.filterByColor = True
+    params.blobColor = 255
+    params.filterByArea = False
+    params.minArea = 1
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    #Find the pivot point (Star closest to the center)
+    i = 2
+    stars_coordinate = []
+    while True:
+        top = y-i
+        down = y+i
+        vertical = abs(top-down)
+        if vertical >= height:
+            new_i = height/2
+            top = int(round(y-new_i))
+            down = int(round(y+new_i))
+        left = x-i
+        right = x+i
+        horizontal = abs(left-right)
+        if horizontal >= width:
+            new_i = width/2
+            left = int(round(x-new_i))
+            right =int(round(x+new_i))
+        croppedimg = img[top:down,left:right,:]
+        y_crop,x_crop,col = croppedimg.shape
+        keypoints = detector.detect(croppedimg)
+        print(len(keypoints))
+        if len(keypoints) > 3:
+            print("Cropped size: \nx: {}\ny: {}".format(x_crop,y_crop))
+            print("Full size: \nx: {}\ny: {}".format(width,height))
+            for index,keypoint in enumerate(keypoints):
+                x_centralstar_crop = int(round(keypoints[index].pt[0]))
+                y_centralstar_crop = int(round(keypoints[index].pt[1]))
+                print("Crop Coordinates: \nx: {}\ny: {}".format(x_centralstar_crop,y_centralstar_crop))
+                coord_x_centralstar = int(round(x_centralstar_crop + ((width-x_crop)/2)))
+                coord_y_centralstar = int(round(y_centralstar_crop + ((height-y_crop)/2)))
+                stars_coordinate.append([coord_x_centralstar,coord_y_centralstar])
+                print("COORDINATES: \n","x: ",coord_x_centralstar,"\n","y: ",coord_y_centralstar)
+            break
+        i+=2
+
+    #Draw circles on important stars and find the pivot star
+    dist_to_center = []
+    for coord in stars_coordinate:
+        center = (coord[0],coord[1])
+        dist_x_to_center = abs(coord[0] - x)
+        dist_y_to_center = abs(coord[1] - y)
+        resultant = (dist_x_to_center**2+dist_y_to_center**2)**1/2
+        dist_to_center.append(resultant)
+        cv2.circle(img,center,2,(255,0,0),2)
+    
+    return img
---
+++ conv-net_initial-results/raw_train/evaluate.py
@@ -0,0 +1,70 @@
+#Load model
+from keras.models import load_model
+model = load_model('raw_train_model.h5')
+
+#Predict function
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+import time
+from keras import backend as K
+
+def predict_class(class_no):
+    directory = 'dataset/test/'
+    class_no = str(class_no)+'/'
+    path = os.path.join(directory,class_no)
+    predict_results = []
+    time_total = []
+    for filename in os.listdir(path):
+        start_time = time.time()
+        test_image = image.load_img(
+            os.path.join(path,filename),
+            target_size=(64,64)
+            )
+        test_image = image.img_to_array(test_image)
+        test_image = test_image/test_image.max()
+        test_image = np.expand_dims(test_image,axis=0)
+        result = model.predict(test_image)
+        result = result.reshape(8)
+        final_time = time.time()
+        delta_t = final_time - start_time
+        time_total.append(delta_t)
+        predict_results.append(result)
+    return predict_results,time_total
+
+def get_accuracy(predict_results,class_no):
+    right = 0
+    wrong = 0
+    not_fully_confident = 0
+    for result in predict_results:
+        if result[class_no] == 1:
+            right+=1
+        else:
+            wrong+=1
+        for neuron in result:
+            if neuron == 1:
+                continue
+            elif neuron != 0:
+                not_fully_confident+=1
+                break
+    accuracy = right/(right+wrong)
+    return accuracy,not_fully_confident
+
+results = []
+accuracy_list = []
+time_ave = []
+for i in range(8):
+    result,time_perclass = predict_class(i)
+    results.append(result)
+    time_ave.append(sum(time_perclass)/3600)
+    accuracy,confidence_number = get_accuracy(results[i],i)
+    accuracy_list.append(accuracy)
+    print("The accuracy for class {} is: ".format(i),accuracy)
+    print("There are {} predictions that are not 100 percent confident".format(confid))
+
+ave_accuracy = sum(accuracy_list)/len(accuracy_list)
+ave_time = sum(time_ave)/len(time_ave)
+print("Total accuracy: ",ave_accuracy)
+print("Average time: ",ave_time)
